8d4a35eb34a2d41539af5670573aa1e0d9bf994e
Gulam Qasim <gfram@hotmail.co.uk>
2020-01-12 14:19:09 +0000

    Adding Comment to Class

--- WeatherAPI/OpenWeatherMap_Forecast/Data_Handling/OpenWeatherMapModel.cs
+++ WeatherAPI/OpenWeatherMap_Forecast/Data_Handling/OpenWeatherMapModel.cs
@@ -6,8 +6,14 @@ namespace WeatherAPI.OpenWeatherMap_Forecast.Data_Handling
 {
     public class OpenWeatherMapList
     {
+        //Generating Data 1
         public int dt { get; set; }
         public Main main { get; set; }
         public List<Weather> weather { get; set; }
+        public Clouds cloud { get; set; }
+        public Wind wind { get; set; }
+        public Rain rain { get; set; }
+        public System sys { get; set; }
+        public string dt_txt { get; set; }
     }
 }
