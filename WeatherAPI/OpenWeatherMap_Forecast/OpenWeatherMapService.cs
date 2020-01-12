ec453803306cca7125b1b1915433e32cc0536238
Gulam Qasim <gfram@hotmail.co.uk>
2020-01-12 13:58:38 +0000

    Calling Data to Service

--- WeatherAPI/OpenWeatherMap_Forecast/OpenWeatherMapService.cs
+++ WeatherAPI/OpenWeatherMap_Forecast/OpenWeatherMapService.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WeatherAPI.OpenWeatherMap_Forecast.Data_Handling;
+using WeatherAPI.OpenWeatherMap_Forecast.HTTP_Manager;
 
 namespace WeatherAPI.OpenWeatherMap_Forecast
 {
     class OpenWeatherMapService
     {
+        //We need DTO
+        public OpenWeatherMapDTO openWeatherMapDTO = new OpenWeatherMapDTO();
+        //We need Call Manager
+        public OpenWeatherMapCallManager openWeatherMapCall = new OpenWeatherMapCallManager();
     }
 }
