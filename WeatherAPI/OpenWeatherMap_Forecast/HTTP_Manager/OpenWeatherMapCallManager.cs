8f830f1b0d6d82c847ea39866870f71a0a7f4e2e
unknown <gfram@hotmail.com>
2020-01-12 13:41:44 +0000

    Finished Adding Stucture

---
+++ WeatherAPI/OpenWeatherMap_Forecast/HTTP_Manager/OpenWeatherMapCallManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAPI.OpenWeatherMap_Forecast.HTTP_Manager
+{
+    class OpenWeatherMapCallManager
+    {
+    }
+}
