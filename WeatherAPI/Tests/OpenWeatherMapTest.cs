5db3bf8223b97e629d7d9dbf29219d28d8c5d199
Gulam Qasim <gfram@hotmail.co.uk>
2020-01-13 19:10:28 +0000

    Added WeatherAPI Namespace to Test

--- WeatherAPI/Tests/OpenWeatherMapTest.cs
+++ WeatherAPI/Tests/OpenWeatherMapTest.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
+using WeatherAPI.OpenWeatherMap_Forecast;
 
 namespace WeatherAPI.Tests
 {
     [TestFixture]
     public class OpenWeatherMapTest
     {
+        private OpenWeatherMapService openWeatherMapForecase = new OpenWeatherMapService();
+
         [Test]
         public void TestMethod1()
         {
