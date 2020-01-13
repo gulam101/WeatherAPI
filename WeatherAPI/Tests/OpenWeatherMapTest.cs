438a28c1161026f4c7b334a7c90a49fd3c659200
Gulam Qasim <gfram@hotmail.co.uk>
2020-01-13 21:47:27 +0000

    Testing for Message

--- WeatherAPI/Tests/OpenWeatherMapTest.cs
+++ WeatherAPI/Tests/OpenWeatherMapTest.cs
@@ -16,5 +16,15 @@ namespace WeatherAPI.Tests
             //Testing the internal paramter for code and paramater stay the same
             NUnit.Framework.Assert.AreEqual("200", openWeatherMapForecast.openWeatherMapDTO.openWeatherMap.cod);
         }
+
+        [Test]
+        public void OpenWeatherMapMessage()
+        {
+            //internal paramater changes. therefore the test of message will be to check
+            //if the message is greater than 0.0
+            NUnit.Framework.Assert.Greater(openWeatherMapForecast.openWeatherMapDTO.openWeatherMap.message, 0.0);
+        }
+
+
     }
 }
