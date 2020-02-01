37129b6922e611d9cd0df89cbf2a9bf9440529ec
Gulam Qasim <gfram@hotmail.co.uk>
2020-02-01 14:40:46 +0000

    Added Lists to OpenWeatherMapListWeatherID

--- WeatherAPI/Tests/OpenWeatherMapTest.cs
+++ WeatherAPI/Tests/OpenWeatherMapTest.cs
@@ -94,6 +94,10 @@ namespace WeatherAPI.Tests
             var rain = new List<int>();
             //list for rain id
             rain.AddRange(new[] { 500, 501, 502, 503, 504, 511, 520, 521, 522, 531 });
+            //lists for cloud id
+            rain.AddRange(new[] { 801, 802, 803, 804 });
+            //lists for thunder id
+            rain.AddRange(new[] { 200, 201, 202, 211, 212, 221, 230, 231, 232 });
         }
 
     }
